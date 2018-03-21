141fda8f7f1d57f53c4981981c93528113b3a3dc
xudong <xudong@xmind.net>
2018-03-21 12:24:04 +0800

    Initial Commit

---
+++ XSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XSlider
+//
+//  Created by Xudong on 2018/3/21.
+//  Copyright © 2018年 Evenki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
